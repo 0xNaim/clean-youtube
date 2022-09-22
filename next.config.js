6b6d7ebb0281b505ab33b4bab46c76190829e7d8
0xNaim <naimislam3545@gmail.com>
2022-09-23 02:08:41 +0600

    feat: pwa implemented

--- next.config.js
+++ next.config.js
@@ -2,16 +2,16 @@
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  },
   reactStrictMode: true,
   swcMinify: true,
   env: { NEXT_PUBLIC_YOUTUBE_API_KEY: process.env.NEXT_PUBLIC_YOUTUBE_API_KEY },
   images: {
     domains: ['i.ytimg.com'],
     formats: ['image/avif', 'image/webp'],
+  },
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
   },
 });
--- pages/_document.js
+++ pages/_document.js
@@ -12,8 +12,8 @@ const Document = () => {
         />
         <meta name='description' content='Clean YouTube' />
         <meta name='theme-color' content='#fff' />
-        <link rel='manifest' href='manifest.json' />
-        <link rel='apple-touch-icon' href='/icon.png'></link>
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='apple-touch-icon' href='/apple-touch-icon.png'></link>
         <link
           rel='stylesheet'
           href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
