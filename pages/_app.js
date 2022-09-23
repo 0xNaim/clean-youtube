0b0b6981ac82c81f6be72cae6d69aca5eafff598
0xNaim <naimislam3545@gmail.com>
2022-09-24 05:15:46 +0600

    feat: recent playlist added

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { persistor, store } from '../redux/store';
 import '../styles/globals.scss';
+import 'react-multi-carousel/lib/styles.css';
 
 function MyApp({ Component, pageProps }) {
   return (
